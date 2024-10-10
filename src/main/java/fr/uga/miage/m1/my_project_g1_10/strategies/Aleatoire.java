64cf762fee996d6a88cd8e74755d7e5aa76f40b6
Alaoui Douiri Ismail <i.alaouidouiri@gmail.com>
2024-10-10 15:36:47 +0200

    correct random method issue

--- src/main/java/fr/uga/miage/m1/my_project_g1_10/strategies/Aleatoire.java
+++ src/main/java/fr/uga/miage/m1/my_project_g1_10/strategies/Aleatoire.java
@@ -2,11 +2,14 @@ package fr.uga.miage.m1.my_project_g1_10.strategies;
 
 import fr.uga.miage.m1.my_project_g1_10.model.Joueur;
 
-import java.util.Random;
+import java.security.SecureRandom;
 
 public class Aleatoire implements Strategie {
+    private static final SecureRandom secureRandom = new SecureRandom();
+
     @Override
     public boolean decider(Joueur adversaire) {
-        return new Random().nextBoolean();
+        boolean decision = secureRandom.nextBoolean();
+        return decision;
     }
 }
